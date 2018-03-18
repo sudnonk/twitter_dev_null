85b106de36743f3ebb3bcc6ffbbc53c0505c9f96
しとざわ <sudnonk12@yahoo.co.jp>
2018-03-18 19:23:21 +0900

    起動時に起動メッセージを表示し、そのメッセージ以降のツイートを削除するように変更

--- main.php
+++ main.php
@@ -4,15 +4,42 @@
 
     use Abraham\TwitterOAuth\TwitterOAuth;
 
-    $connection      = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET);
+    $connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_SECRET);
 
-    $latest_tweet_id = "975248769649278976";
-    while(true) {
-        $content         = $connection->get("statuses/user_timeline", array("user_id" => "3164304848", "count" => "1","since_id"=>$latest_tweet_id));
-        if(isset($content[0])){
-            $latest_tweet_id = (string)$content[0]->{"id"};
-            var_dump($content[0]->{"id"});
-            $delete = $connection->post("statuses/destroy", array("id" => $latest_tweet_id));
+    //ここから先のツイートは削除されます
+    $connection->post("statuses/update", array("status" => "起動"));
+    $criteria = get_last_tweet_ids($connection, 1)[0];
+
+    while (true) {
+        $last_tweet_ids = get_last_tweet_ids($connection, 10, $criteria);
+        foreach ($last_tweet_ids as $last_tweet_id) {
+            $connection->post("statuses/destroy", array("id" => $last_tweet_id));
         }
-        sleep(2);
+        sleep(1.5);
+    }
+
+    /**
+     * $since_idを含まずに$since_idより新しいツイートのうち最新$num個のツイートを得る
+     *
+     * @param TwitterOAuth $connection
+     * @param int          $num
+     * @param string       $since_id
+     *
+     * @return array 見つからなかったら空配列
+     */
+    function get_last_tweet_ids(TwitterOAuth $connection, int $num, string $since_id = ""): array {
+        $array = array("user_id" => MY_USER_ID, "count" => "$num",);
+
+        if ($since_id !== "") {
+            $array["since_id"] = $since_id;
+        }
+
+        $contents = $connection->get("statuses/user_timeline", $array);
+
+        $results = array();
+        foreach ($contents as $content) {
+            $results[] = $content->{"id"};
+        }
+
+        return $results;
     }
